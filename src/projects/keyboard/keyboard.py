0d1b321fcaf254e51ef7a81617c4cb7b6d63535c
banual01 <banual01@luther.edu>
2021-02-26 20:38:24 -0600

    working

--- src/projects/keyboard/keyboard.py
+++ src/projects/keyboard/keyboard.py
@@ -16,8 +16,6 @@ def spell_check(filename: str) -> None:
             total_test = int(line[1])
             for idx in range(total_test):
                 word = r.readline()[:-1]
-
-
                 scores = {}
                 score = word_score(target,word)
                 if score not in scores.keys():
@@ -30,40 +28,40 @@ def spell_check(filename: str) -> None:
                     print(word, sorted_key)
 
 
-    keyboard_dict = {
-        1: {"q":1,"w":2,"e":3,"r":4,"t":5,"y":6,"u":7,"i":8,"o":9,"p":10},
-        2: {"a":1,"s":2,"d":3,"f":4,"g":5,"h":6,"j":7,"k":8,"l":9},
-        3: {"z":1,"x":2,"c":3,"v":4,"b":5,"n":6,"m":7}
-    }
+keyboard_dict = {
+    1: {"q":1,"w":2,"e":3,"r":4,"t":5,"y":6,"u":7,"i":8,"o":9,"p":10},
+    2: {"a":1,"s":2,"d":3,"f":4,"g":5,"h":6,"j":7,"k":8,"l":9},
+    3: {"z":1,"x":2,"c":3,"v":4,"b":5,"n":6,"m":7}
+}
     #second_strings = ["iopc", "icpc", "gcpc"]
     
-    def word_score(target, word):
-        score = 0
-        for idx in range (len(target)):
-            first_letter = target[idx]
-            second_letter = word[idx]
-            vertical = 0
-            horizontal = 0
-            row1 = 0
-            row2 = 0
+def word_score(target, word):
+    score = 0
+    for idx in range (len(target)):
+        first_letter = target[idx]
+        second_letter = word[idx]
+        vertical = 0
+        horizontal = 0
+        row1 = 0
+        row2 = 0
 
-            for first_row in keyboard_dict.keys():
-                if first_letter in keyboard_dict[first_row].keys():
-                    row1 = first_row
-            
-            for second_row in keyboard_dict.keys():
-                if second_letter in keyboard_dict[second_row].keys():
-                    row2 = second_row
-
-            horizontal = abs(keyboard_dict[row1][first_letter] - keyboard_dict[row2][second_letter])
-            vertical = abs(row1-row2)
-
-            score += horizontal
-            score += vertical  
+        for first_row in keyboard_dict.keys():
+            if first_letter in keyboard_dict[first_row].keys():
+                row1 = first_row
         
-        return(score)
-        scores[sorted_key].sort()
-    #for word in words:
+        for second_row in keyboard_dict.keys():
+            if second_letter in keyboard_dict[second_row].keys():
+                row2 = second_row
+
+        horizontal = abs(keyboard_dict[row1][first_letter] - keyboard_dict[row2][second_letter])
+        vertical = abs(row1-row2)
+
+        score += horizontal
+        score += vertical  
+    
+    return(score)
+    #scores[sorted_key].sort()
+
     
 
 
