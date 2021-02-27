cd77fc04cd90d3f12616a661822f89bfc0057c7d
banual01 <banual01@luther.edu>
2021-02-26 20:18:20 -0600

    working

--- src/projects/keyboard/keyboard.py
+++ src/projects/keyboard/keyboard.py
@@ -7,19 +7,27 @@
 
 def spell_check(filename: str) -> None:
     """Rank words by their proximity to the target"""
-    
+
+    with open(filename, "r") as r:
+        inputcase = int(r.readline())
+        for inputs in range(inputcase):
+            line = r.readline().split(" ")
+            target = line[0]
+            total_test = int(line[1])
+
+
+
     keyboard_dict = {
         1: {"q":1,"w":2,"e":3,"r":4,"t":5,"y":6,"u":7,"i":8,"o":9,"p":10},
         2: {"a":1,"s":2,"d":3,"f":4,"g":5,"h":6,"j":7,"k":8,"l":9},
         3: {"z":1,"x":2,"c":3,"v":4,"b":5,"n":6,"m":7}
     }
-    first_string = "ifpv"
     second_strings = ["iopc", "icpc", "gcpc"]
     
-    def word_score(first_string, second_string):
+    def word_score(target, second_string):
         score = 0
-        for idx in range (len(first_string)):
-            first_letter = first_string[idx]
+        for idx in range (len(target)):
+            first_letter = target[idx]
             second_letter = second_string[idx]
             vertical = 0
             horizontal = 0
@@ -39,13 +47,13 @@ def spell_check(filename: str) -> None:
 
             score += horizontal
             score += vertical  
-
+        
         return(score)
-
+        scores[sorted_key].sort()
     scores = {}
     for second_string in second_strings:
-        score = word_score(first_string,second_string)
-        if word_score(first_string,second_string) not in scores.keys():
+        score = word_score(target,second_string)
+        if word_score(target,second_string) not in scores.keys():
             scores[score] = [second_string]
         else:
             scores[score].append(second_string)
