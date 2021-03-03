5ec1c7249cc79a559d3f66b84583355847290c99
banual01 <banual01@luther.edu>
2021-03-03 14:24:31 -0600

    working

--- src/projects/rpn/rpn.py
+++ src/projects/rpn/rpn.py
@@ -24,18 +24,32 @@ class TokenError(Exception):
         Exception.__init__(self, *args, **kwargs)
 
 
-op_expr = {"+", "-", "*", "/"}
 
 def postfix_eval(postfix_expr: str) -> int:
     # TODO: Evaluate an expression
     eval_stack = postfix_expr.spilt()
+    math_stack = Stack()
     for str_index in range(len(eval_stack)):
-        try:
-            int(eval_stack[str_index])
-            float(eval_stack[str_index])
-            return postfix_expr
-        except:
-            raise TokenError(f"Unknown token: {eval_stack[str_index]}")
+        if eval_stack[str_index] not in {"+", "-", "*", "/"}:
+            try:
+                int(eval_stack[str_index])
+                float(eval_stack[str_index])
+                operation = int(eval_stack[str_index])
+                math_stack.push(operation)
+            except:
+                raise TokenError(f"Unknown token: {eval_stack[str_index]}")
+        else: 
+            try:
+                op2 = math_stack.pop()
+                op1 = math_stack.pop()
+                math_stack.push(do_math(eval_stack[str_index], op1, op2))
+            except:
+                raise StackError("Stack is empty")    
+    if math_stack.size() > 1:
+        raise StackError("Stack is not empty")
+    return math_stack.peek()
+
+
 
 
 def do_math(op: str, op1: int, op2: int) -> int:
@@ -57,19 +71,4 @@ def rpn_calc(filename: str) -> int:
         expr = r.readline()
         postfix_expr = postfix_eval(expr)
     
-    stack = postfix_expr.split()
-    math_stack = Stack()
-    for str_index in range(len(stack)):
-        if stack[str_index] not in op_expr:
-            operation = int(stack[str_index])
-            math_stack.push(operation)
-        else: 
-            try:
-                op2 = math_stack.pop()
-                op1 = math_stack.pop()
-                math_stack.push(do_math(stack[str_index], op1, op2))
-            except:
-                raise StackError("Stack is empty")    
-    if math_stack.size() > 1:
-        raise StackError("Stack is not empty")
-    return math_stack.peek()
+
