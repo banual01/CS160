878c8715464f4586d1596646fbe1dd399d30e576
banual01 <banual01@luther.edu>
2021-03-06 15:58:03 -0600

    working

--- src/projects/bank/bank_classes.py
+++ src/projects/bank/bank_classes.py
@@ -15,23 +15,52 @@ class Address:
         self, street_init: str, city_init: str, state_init: str, zip_init: str
     ):
         """__init__"""
-        self.street = street_init
-        self.city = city_init
-        self.state = state_init
-        self.zip = zip_init
+        self._street = street_init
+        self._city = city_init
+        self._state = state_init
+        self._zip = zip_init
 
     # TODO: Implement data members as properties
+
+    @property
+    def street(self):
+        """Get the street"""
+        return self._street
+
+    @property
+    def city(self):
+        """Get the city"""
+        return self._city
+
+    @property
+    def state(self):
+        """Get the state"""
+        return self._state
+
+    @property
+    def zip(self):
+        """Get the zip"""
+        return self._zip
+
 
     def __eq__(self, other: object):
         """Compare 2 addresses"""
         try:
-            self.address == other.address
+            self._street == other._street
+            self._city == other._city
+            self._state == other._state
+            self._zip == other._zip
+            return self.address and other.address
         except:
-            self.address is not other.address
+            self._street != other._street
+            self._city != other._city
+            self._state != other._state
+            self._zip != other._zip
+            return None
 
     def __str__(self):
         """__str method"""
-        return f"{self.street}\n{self.city}, {self.state} {self.zip}"
+        return f"{self._street}\n{self._city}, {self._state} {self._zip}"
 
 
 class Customer:
@@ -39,19 +68,36 @@ class Customer:
 
     def __init__(self, name_init: str, dob_init: str, address_init: object):
         """Constructor"""
-        self.name = name_init
-        self.dob = dob_init
-        self.address = address_init
+        self._name = name_init
+        self._dob = dob_init
+        self._address = address_init
 
     # TODO: Implement data members as properties
 
+    @property
+    def name(self):
+        """Get the name"""
+        return self._name
+
+    @property
+    def dob(self):
+        """Get the dob"""
+        return self._dob
+
+    @property
+    def address(self):
+        """Get the address"""
+        return self._address
+
+
     def move(self, new_address: object):
         """Change address"""
-        raise NotImplementedError
+        new_address = Address(self._address)
+        return new_address
 
     def __str__(self):
         """__str"""
-        return f"{self.name} ({self.dob})\n{str(self.address)}"
+        return f"{self._name} ({self._dob})\n{str(self._address)}"
 
 
 class Account(ABC):
@@ -60,21 +106,34 @@ class Account(ABC):
     @abstractmethod
     def __init__(self, owner_init: object, balance_init: float = 0):
         """Constructor"""
-        self.owner = owner_init
-        self.balance = balance_init
+        self._owner = owner_init
+        self._balance = balance_init
 
     # TODO: Implement data members as properties
+
+    @property
+    def owner(self):
+        """Get the owner"""
+        return self._owner
+
+    @property
+    def balance(self):
+        """Get the balance"""
+        return self._balance
+
 
     def deposit(self, amount: float):
         """Add money"""
         if amount >= 0:
-            return self.balance + amount
+            self._balance += amount
         else:
             raise ValueError("Must deposit positive amount")
 
     def close(self):
         """Close account"""
-        raise NotImplementedError
+        if self._balance < 0:
+            closed_account = self._balance = 0
+            return round(closed_account, 2)
 
     def __str__(self):
         """__str__"""
@@ -91,11 +150,16 @@ class CheckingAccount(Account):
 
     def process_check(self, amount: float):
         """Process a check"""
-        return Account.deposit(self, amount)
+        if self._balance >= amount:
+            new_checamount = self._balance - amount
+            return round(new_checamount, 2)
+        else:
+            new_feeamount = self._balance - self._fee
+            return round(new_feeamount, 2)
 
     def __str__(self):
         """__str__"""
-        return f"Checking account\nOwner: {self.owner}\nBalance: {self.balance:.2f}"
+        return f"Checking account\nOwner: {self._owner}\nBalance: {self._balance:.2f}"
 
 
 class SavingsAccount(Account):
@@ -109,8 +173,9 @@ class SavingsAccount(Account):
 
     def yield_interest(self):
         """Yield annual interest"""
-        raise NotImplementedError
+        new_interaccount = self._balance * (1 + self._interest_rate / 100)
+        return round(new_interaccount, 2)
 
     def __str__(self):
         """__str__"""
-        return f"Savings account\nOwner: {self.owner}\nBalance: {self.balance:.2f}"
+        return f"Savings account\nOwner: {self._owner}\nBalance: {self._balance:.2f}"
