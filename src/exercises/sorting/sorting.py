206ff72323e40360558cb6eb9ae5d0f07694c4b6
banual01 <banual01@luther.edu>
2021-03-30 08:34:52 -0500

    needs modifications

---
+++ src/exercises/sorting/sorting.py
@@ -0,0 +1,95 @@
+def selection_sort(a_list):
+    for i, item in enumerate(a_list):
+        min_idx = len(a_list) - 1
+        for j in range(i, len(a_list)):
+            if a_list[j] < a_list[min_idx]:
+                min_idx = j
+        if min_idx != i:
+            a_list[min_idx], a_list[i] = a_list[i], a_list[min_idx]
+        print(a_list)
+
+
+def insertion_sort(a_list):
+    for i in range(1, len(a_list)):
+        cur_val = a_list[i]
+        cur_pos = i
+
+        while cur_pos > 0 and a_list[cur_pos - 1] > cur_val:
+            a_list[cur_pos] = a_list[cur_pos - 1]
+            cur_pos = cur_pos - 1
+        a_list[cur_pos] = cur_val
+        print(a_list)
+
+
+def quick_sort(a_list):
+    quick_sort_helper(a_list, 0, len(a_list) - 1)
+
+
+def quick_sort_helper(a_list, first, last):
+    if first < last:
+        split = partition(a_list, first, last)
+        quick_sort_helper(a_list, first, split - 1)
+        quick_sort_helper(a_list, split + 1, last)
+
+
+def partition(a_list, first, last):
+    pivot_val = a_list[first]
+    left_mark = first + 1
+    right_mark = last
+    done = False
+
+    while not done:
+        while left_mark <= right_mark and a_list[left_mark] <= pivot_val:
+            left_mark = left_mark + 1
+        while left_mark <= right_mark and a_list[right_mark] >= pivot_val:
+            right_mark = right_mark - 1
+        if right_mark < left_mark:
+            done = True
+        else:
+            a_list[left_mark], a_list[right_mark] = (
+                a_list[right_mark],
+                a_list[left_mark],
+            )
+    a_list[first], a_list[right_mark] = a_list[right_mark], a_list[first]
+    print(a_list)   
+    return right_mark
+
+
+def merge_sort(a_list):
+    print("Splitting", a_list)
+    if len(a_list) > 1:
+        mid = len(a_list) // 2
+        left_half = a_list[:mid]
+        right_half = a_list[mid:]
+
+        merge_sort(left_half)
+        merge_sort(right_half)
+
+        i, j, k = 0, 0, 0
+        while i < len(left_half) and j < len(right_half):
+            if left_half[i] <= right_half[j]:
+                a_list[k] = left_half[i]
+                i = i + 1
+            else:
+                a_list[k] = right_half[j]
+                j = j + 1
+            k = k + 1
+
+        while i < len(left_half):
+            a_list[k] = left_half[i]
+            i = i + 1
+            k = k + 1
+
+        while j < len(right_half):
+            a_list[k] = right_half[j]
+            j = j + 1
+            k = k + 1
+    print("Merging", a_list)
+
+selection_sort([15, 16, 73, 65, 38, 10, 22, 79, 87, 64])
+print()
+# insertion_sort([15, 16, 73, 65, 38, 10, 22, 79, 87, 64])
+# print()
+# quick_sort([15, 16, 73, 65, 38, 10, 22, 79, 87, 64])
+# print()
+# merge_sort([15, 16, 73, 65, 38, 10, 22, 79, 87, 64])
